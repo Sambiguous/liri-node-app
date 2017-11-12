30dd5149d5c0c2fb7f525121d6526eccc8237f48
Sam <sjdefrank91@gmail.com>
2017-11-11 20:43:57 -0600

    finish twitter functionality

--- liri.js
+++ liri.js
@@ -1,19 +1,60 @@
 var keys = require("./keys.js");
 var request = require("request");
+var Twitter = require("twitter");
 
-var command = process.argv.slice(2, process.argv.length).join(" ").toLowerCase();
+
+var command = process.argv[2].toLowerCase();
+
+
+function formatTweet(number, timestamp, tweet){
+    var num = number.toString() + ".";
+    var line = 1;
+    var time = "\t" + timestamp.split(" ").slice(0, 4).join(" ") + " UTC\n";
+    var text = "\t";
+
+    var arr = tweet.replace("\n", "\n\t").split("https://")[0].split(" ");
+
+    for(var i in arr){
+        if((text.length + arr[i].length) > (80 * line)){
+            text += "\n\t";
+            line++;
+        };
+        text += arr[i] + " ";
+    };
+
+    return num + time + text;
+};
 
 function liri(cmd){
-    if(cmd = "my tweets"){
-        var url = "https://"
-        request("https://api.twitter.com/1.1/search/tweets.json", function(error, response, body){
-            console.log(error);
-            console.log("===================================");
-            console.log(body);
+    console.log("");
+    if(cmd = "mytweets"){
+        var client = new Twitter(keys);
+        var params = {
+            screen_name: "realDonaldTrump",
+            count: 20,
+            include_rts: true,
+            trim_user: true,
+            tweet_mode: "extended"
+        };
         
-        })
+        client.get("statuses/user_timeline", params, function(error, tweets, response){
+            if(error){
+                console.log(error);
+            }
+            else{
+                for(i in tweets){
+                    var tweet = formatTweet(parseInt(i) + 1, tweets[i].created_at, tweets[i].full_text);
+                    console.log(tweet);
+                    console.log("");
+                    console.log("");
+                };
+            };
+        });
     }
+
 };
 
 
 liri(command);
+
+
